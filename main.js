3fcc040a181690a4420bca2249464094293df4f8
Michelle Lim <lim.miche@northeastern.edu>
2022-02-15 22:14:46 -0500

    circle 1 :D

---
+++ main.js
@@ -0,0 +1,18 @@
+/*
+
+HW-04
+Kreena Totala & Michelle Lim
+2/15/22
+
+*/
+
+clickCount = 0;
+function circle1Clicked() {
+    //console.log("Button 1 was clicked"); 
+    clickCount++;
+    // Set innerHTML of div
+    document.getElementById("click").innerHTML = clickCount; 
+}
+
+
+
