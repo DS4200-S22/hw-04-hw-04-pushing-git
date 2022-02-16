be77287c1160142b0429962a6831d6c1b3625b29
Michelle Lim <lim.miche@northeastern.edu>
2022-02-15 23:08:46 -0500

    DONE :DDD

--- main.js
+++ main.js
@@ -6,6 +6,7 @@ Kreena Totala & Michelle Lim
 
 */
 
+/* Circle #1 */
 clickCount = 0;
 function circle1Clicked() {
     //console.log("Button 1 was clicked"); 
@@ -14,24 +15,23 @@ function circle1Clicked() {
     document.getElementById("click").innerHTML = clickCount; 
 }
 
-
-
+/* Circle #2 */
 function showBorder() {
     let circle2 = document.getElementById("circle2");
     circle2.setAttribute("stroke", "black");
-  }
+}
   
-  function hideBorder() {
-    let circle2 = document.getElementById("circle2");
-    circle2.setAttribute("stroke", "white");
-  }
+function hideBorder() {
+  let circle2 = document.getElementById("circle2");
+  circle2.setAttribute("stroke", "white");
+}
 
-
-  function disappear() {
-    let element = document.getElementById("circle3");
-    if (element.style.opacity == 0) {
-      element.style.opacity = 1;
-    } else {
-      element.style.opacity = 0;
-    }
-  }
+/* Circle #3 */
+function disappear() {
+   let element = document.getElementById("circle3");
+   if (element.style.opacity == 0) {
+     element.style.opacity = 1;
+   } else {
+     element.style.opacity = 0;
+   }
+ }
